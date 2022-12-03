1d0109c57d502dbeb1e7df349d3f1a90e61dc851
Melina Tevez <melinatevez15@gmail.com>
2022-12-03 18:52:43 -0300

    Agrego primeras carpetas y archivos. Agrego rutas. Comienzo con controllers.

---
+++ controller/productos.js
@@ -0,0 +1,19 @@
+const obtenerProductos = (req, res) => {
+    res.send('Soy un controlador re piola')
+}
+const guardarProducto = (req, res) => {
+    res.send('Soy un controlador re piola')
+}
+const actualizarProducto = (req, res) => {
+    res.send('Soy un controlador re piola')
+}
+const borrarProducto = (req, res) => {
+    res.send('Soy un controlador re piola')
+}
+
+module.exports = {
+    obtenerProductos,
+    guardarProducto,
+    actualizarProducto,
+    borrarProducto
+}
