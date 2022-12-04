d5a6099ec18472523c5b978ac8b731c95bfc6ebd
Melina Tevez <melinatevez15@gmail.com>
2022-12-04 12:57:19 -0300

    Comienzo con el CRUD(read one/all y create)

--- controller/productos.js
+++ controller/productos.js
@@ -1,14 +1,27 @@
-const obtenerProductos = (req, res) => {
-    res.send('Soy un controlador re piola')
+const service = require('../service/productos')
+
+const obtenerProductos = async (req, res) => {
+    let id = req.params.id
+    
+    if(id) {
+        const producto = await service.obtenerProducto(id)
+        return res.status(200).json(producto)
+    } else {
+        const productos = await service.obtenerProductos()
+        return res.status(200).json(productos)
+    }
+
 }
-const guardarProducto = (req, res) => {
-    res.send('Soy un controlador re piola')
+const guardarProducto = async (req, res) => {
+    const producto = req.body
+    const productoGuardado = await service.guardarProducto(producto)
+    res.status(201).json(productoGuardado)
 }
 const actualizarProducto = (req, res) => {
-    res.send('Soy un controlador re piola')
+    res.send('Controlador')
 }
 const borrarProducto = (req, res) => {
-    res.send('Soy un controlador re piola')
+    res.send('Controlador')
 }
 
 module.exports = {
---
+++ model/productos.js
@@ -0,0 +1,21 @@
+const ProductoModelMongoDB = require('./productosMongoDB')
+
+class ProductoModel {
+    static get(tipo){
+
+        switch(tipo){
+            case 'MONGODB':
+                console.log('**** PRESISTENCIA EN MONGODB (productos) ****')
+                const mongodb = new ProductoModelMongoDB()
+                mongodb.conectarDB()
+                return mongodb
+
+            default:
+                console.log('No paso ningún tipo')
+                break
+
+        }
+    }
+}
+
+module.exports = ProductoModel
